c6d18156746a9c811d28d244a6db5baed0413c02
Kusuma31Reddy <kusumareddy888@gmail.com>
2024-09-27 20:08:37 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,4 +1,4 @@
-  public class demo {
+  public class  demo {
     public static void main(String[] args) {
         // Print a message to the console as output
         System.out.println("Hello welcome to devops");
